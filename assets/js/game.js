74e76fc53ac2eb5e6ac8853e60e338b309b84d89
fghoneycutt <fghoneycutt@gmail.com>
2022-06-19 17:03:58 -0400

    randomized attack order

--- assets/js/game.js
+++ assets/js/game.js
@@ -32,16 +32,21 @@ promptFight = promptFight.toLowerCase();
 }
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
+  // keep track of who goes first
+  var isPlayerTurn=true;
+  if (Math.random()> 0.5){
+    isPlayerTurn=false;
+  }
   while (playerInfo.health > 0 && enemy.health > 0) {
-
-// ask player if they'd like to fight or skip using fightOrSkip function
-    if (fightOrSkip()) {
-      // if true, leave fight by breaking loop
-      break;
-    }
+    if (isPlayerTurn){
+      // ask player if they'd like to fight or skip using fightOrSkip function
+      if (fightOrSkip()) {
+        // if true, leave fight by breaking loop
+        break;
+      }
     // generate random damage value based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-
+    // remove enemy's health by subtracting the amount we set in the damage variable
     enemy.health = Math.max(0, enemy.health - damage);
     console.log(
       playerInfo.name + ' attacked ' + enemy.name + '. ' + enemy.name + ' now has ' + enemy.health + ' health remaining.'
@@ -59,16 +64,14 @@ var fight = function(enemy) {
     } else {
       window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
     }
-
-    // remove players's health by subtracting the amount set in the enemy.attack variable
+    //player gets attacked first
+  }else{
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
-
+    // remove players's health by subtracting the amount set in the damage variable
     playerInfo.health = Math.max(0, playerInfo.health - damage);
-    
     console.log(
       enemy.name + ' attacked ' + playerInfo.name + '. ' + playerInfo.name + ' now has ' + playerInfo.health + ' health remaining.'
     );
-
     // check player's health
     if (playerInfo.health <= 0) {
       window.alert(playerInfo.name + ' has died!');
@@ -77,6 +80,8 @@ var fight = function(enemy) {
     } else {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
+  }
+  isPlayerTurn=!isPlayerTurn;
   } // end of while loop
 }; // end of fight function
 
